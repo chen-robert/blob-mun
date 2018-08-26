fc564120ba761d38f5ec0b8dab42ddda725e021b
gamester543 <gamesterrex@gmail.com>
2018-08-26 16:32:09 -0700

    Refactored server/db/index.js

--- src/client/containers/MUNWrapper.js
+++ src/client/containers/MUNWrapper.js
@@ -6,28 +6,39 @@ import { Card, CardContent, Typography, Grid } from "@material-ui/core";
 
 import MUN from "./MUN";
 
-const MUNWrapper = ({ currStateDefined }) => {
-  if (currStateDefined) return <MUN />;
+const MUNWrapper = ({ currStateDefined, history }) => {
+  const getContent = () => {
+    if (currStateDefined) return <MUN />;
+    return (
+      <Grid
+        style={{ height: "100%" }}
+        container
+        direction="row"
+        justify="center"
+        alignItems="center"
+      >
+        <Card style={{ maxWidth: "50vw" }}>
+          <CardContent>
+            <Typography gutterBottom variant="headline" component="h2">
+              Uh oh!
+            </Typography>
+            <Typography component="p">
+              Something messed up. It looks like this committee doesn't exist!
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    );
+  }
   return (
-    <Grid
-      style={{ height: "100%" }}
-      container
-      direction="row"
-      justify="center"
-      alignItems="center"
-    >
-      <ForceSignin />
-      <Card style={{ maxWidth: "50vw" }}>
-        <CardContent>
-          <Typography gutterBottom variant="headline" component="h2">
-            Uh oh!
-          </Typography>
-          <Typography component="p">
-            Something messed up. It looks like this committee doesn't exist!
-          </Typography>
-        </CardContent>
-      </Card>
-    </Grid>
+  <div>
+    <ForceSignin
+      history={history}
+    />
+    {
+      getContent()
+    }
+  </div>
   );
 };
 const MUNWrapperConnector = connect((state, ownProps) => ({
--- src/server/db/index.js
+++ src/server/db/index.js
@@ -5,63 +5,45 @@ import { connectionString } from "./config";
 const client = new Client({ connectionString });
 client.connect();
 
+const handleError = (err, callback) => {
+  console.error(err);
+  callback("Unknown error");
+}
 export const addUser = (name, email, password, callback) => {
   client.query(
     "SELECT * FROM users WHERE username = $1",
-    [name],
-    (err, res) => {
-      if (err) {
-        console.error(err);
-        return callback("Unknown error");
-      }
-      if (res.rows.length > 0) {
-        return callback("Username already exists");
-      }
-
-      client.query(
-        "SELECT * FROM users WHERE email = $1",
-        [email],
-        (err, res) => {
-          if (err) {
-            console.error(err);
-            return callback("Unknown error");
-          }
-          if (res.rows.length > 0) {
-            return callback("Email already exists");
-          }
-
-          bcrypt.hash(password, 10, (err, hash) => {
-            if (err) {
-              console.error(err);
-              return callback("Unknown error");
-            }
-            client.query(
-              "INSERT INTO users(username, email, password) VALUES($1, $2, $3) RETURNING *",
-              [name, email, hash],
-              (err, res) => {
-                if (err) {
-                  console.error(err);
-                  return callback("Unknown error");
-                }
-
-                return callback();
-              }
-            );
-          });
-        }
-      );
+    [name]
+  )
+  .then(res => {
+    if (res.rows.length > 0) {
+      return callback("Username already exists");
     }
-  );
-};
+    return client.query(
+      "SELECT * FROM users WHERE email = $1",
+      [email],
+    );
+  })
+  .then(res => {
+    if (res.rows.length > 0) {
+      return callback("Email already exists");
+    }
+
+    return bcrypt.hash(password, 10);
+  })
+  .then(hash => client.query(
+    "INSERT INTO users(username, email, password) VALUES($1, $2, $3) RETURNING *",
+    [name, email, hash]
+  ))
+  .then(() => callback())
+  .catch(err => handleError(err, callback));
+}
 export const checkLogin = (name, password, callback) => {
+  let data;
   client.query(
     "SELECT * FROM users WHERE username = $1",
-    [name],
-    (err, res) => {
-      if (err) {
-        console.error(err);
-        return callback("Unknown error");
-      }
+    [name]
+  )
+  .then((res) => {
       if (res.rows.length === 0) {
         return callback("Username does not exist");
       }
@@ -69,46 +51,36 @@ export const checkLogin = (name, password, callback) => {
         console.error(`Multiple entries for username ${name}`);
         return callback("Unknown error");
       }
-      const data = res.rows[0];
-      bcrypt.compare(password, data.password, (err, res) => {
-        if (err) {
-          console.error(err);
-          return callback("Unknown error");
-        }
-        if (!res) {
-          return callback("Invalid password");
-        }
-        return callback(null, { id: data.id });
-      });
+      data = res.rows[0];
+      return bcrypt.compare(password, data.password);
+  })
+  .then(res => {
+    if (!res) {
+      return callback("Incorrect password");
     }
-  );
+    return callback(null, { id: data.id });
+  })
+  .catch(err => handleError(err, callback));
 };
 export const saveState = (id, data, callback) => {
-  client.query("DELETE FROM data WHERE user_id = $1", [id]).then(() =>
+  client.query("DELETE FROM data WHERE user_id = $1 RETURNING *", [id])
+  .then(() =>
     client.query(
       "INSERT INTO data(user_id, data) VALUES($1, $2) RETURNING *",
-      [id, data],
-      (err, res) => {
-        if (err) {
-          console.error(err);
-          return callback("Unknown error");
-        }
-        return callback();
-      }
-    )
-  );
+    [id, data])
+  )
+  .then(() => callback())
+  .catch((err) => handleError(err, callback));
 };
 
 export const getSaveData = (id, callback) => {
-  client.query("SELECT * FROM data WHERE user_id = $1", [id], (err, res) => {
-    if (err) {
-      console.error(err);
-      return callback("Unknown error");
-    }
+  client.query("SELECT * FROM data WHERE user_id = $1", [id])
+  .then((res) => {
     if (res.rows.length === 0) {
       return callback(null, { data: null });
     }
     const data = res.rows[0];
     return callback(null, { data: data.data });
-  });
+  })
+  .catch(err => handleError(err, callback));
 };
